10b793bc9098b5ce20e870e03b068358dc5858de
Sergey <sergos5@yandex.ru>
2024-03-23 21:23:08 +0300

    ToDo

---
+++ js/script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const headerButton = document.querySelector('.header-button');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+let toDoData = [];
+const dataLS = JSON.parse(localStorage.getItem('toDoData'));
+
+const render = function(){
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+    toDoData.forEach(function(item, index){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML= '<span class="text-todo">' + item.text +'</span>' +
+            '<div class="todo-buttons">' +
+                '<button class="todo-remove"></button>' +
+                '<button class="todo-complete"></button>' +
+            '</div>';      
+        if(item.completed){
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }       
+
+        li.querySelector('.todo-complete').addEventListener('click', function(){
+            item.completed = !item.completed;
+            render();
+        });
+
+        li.querySelector('.todo-remove').addEventListener('click', function(){            
+            toDoData.splice(index,1);           
+            render();
+        });        
+    });
+    //localStorage.clear();       
+    localStorage.setItem('toDoData', JSON.stringify(toDoData));   
+};
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+    if (headerInput.value.trim() !== ''){
+        const newToDo = {
+            text: headerInput.value,
+            completed: false
+        }
+        toDoData.push(newToDo);
+        headerInput.value = '';
+        render(); 
+    } 
+});
+
+if (dataLS) {
+    toDoData = dataLS;
+    render();
+};
+ 
